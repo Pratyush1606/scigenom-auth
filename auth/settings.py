7e17cc4d3d88bcc60115e2e35e3494b69174cbfb
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-09-06 22:12:54 +0530

    Updated settings file to remove secret keys
    Signed-off-by: Pratyush1606 <jaiswalpratyush2015@gmail.com>

--- auth/settings.py
+++ auth/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0f89*iq!_yi1ttcgr$zpy)0jr+!_utld1@$a@g*pmb-csixkll"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG") != "False"
 
 ALLOWED_HOSTS = []
 
