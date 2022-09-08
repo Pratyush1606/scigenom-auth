846ffbd699188d61a071511d2e8cf868bcf21b29
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-09-08 18:45:49 +0530

    Added generlaised apis and urls
    Signed-off-by: Pratyush1606 <jaiswalpratyush2015@gmail.com>

--- data_app/urls.py
+++ data_app/urls.py
@@ -1,4 +1,26 @@
 from django.urls import path
+from data_app import views
 
 app_name = "data_app"
-urlpatterns = []
+urlpatterns = [
+    path(
+        "add_usr_prof",
+        views.AddData.as_view(data_id="user_profile"),
+        name="add_usr_prof",
+    ),
+    path(
+        "get_usr_prof",
+        views.GetData.as_view(data_id="user_profile"),
+        name="get_usr_prof",
+    ),
+    path(
+        "add_usr_address",
+        views.AddData.as_view(data_id="user_address"),
+        name="add_usr_address",
+    ),
+    path(
+        "get_usr_address",
+        views.GetData.as_view(data_id="user_address"),
+        name="get_usr_address",
+    ),
+]
--- data_app/views.py
+++ data_app/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from data_app.models import UserProfile, UserAddress
+from data_app.serializers import UserProfileSerializer, UserAddressSerializer
 
-# Create your views here.
+
+data_id_model_mapping = {
+    "user_profile": {"model": UserProfile, "serializer": UserProfileSerializer},
+    "user_address": {"model": UserAddress, "serializer": UserAddressSerializer},
+}
+
+
+class AddData(APIView):
+
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    data_id = None
+
+    def post(self, request):
+        user = request.user
+        data = request.data
+        try:
+            data_model_serializer = data_id_model_mapping[self.data_id]
+        except Exception as e:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        # Assigning the user id to the current logged in user
+        data["user_id"] = user.user_id
+
+        serializer = data_model_serializer["serializer"](data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetData(APIView):
+
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    data_id = None
+
+    def get(self, request):
+        # Getting the user
+        user = request.user
+
+        try:
+            data_model_serializer = data_id_model_mapping[self.data_id]
+        except Exception as e:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        if self.data_id == "user_profile":
+            try:
+                # Since user profile is a one to one field, querying using get
+                user_profile = data_model_serializer["model"].objects.get(
+                    user_id=user.user_id
+                )
+            except UserProfile.DoesNotExist:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+
+            serializer = data_model_serializer["serializer"](user_profile)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        elif self.data_id == "user_address":
+            # Since user address is a multi to one field, querying using filter
+            user_addresses = data_model_serializer["model"].objects.filter(
+                user_id=user.user_id
+            )
+            serializer = data_model_serializer["serializer"](user_addresses, many=True)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
