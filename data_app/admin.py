9be7fcffae008b7abc415b871d6e185aa5217c5f
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-09-10 11:17:02 +0530

    Regsitered models to the admin
    Signed-off-by: Pratyush1606 <jaiswalpratyush2015@gmail.com>

--- data_app/admin.py
+++ data_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from data_app.models import UserProfile, UserAddress
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(UserAddress)
